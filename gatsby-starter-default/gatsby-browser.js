501f58f7c41749a2661f2fe895b46269aad72105
Michael Helgesen <michaelhelgesen02@gmail.com>
2021-05-28 13:29:06 +0200

    fonts

--- gatsby-starter-default/gatsby-browser.js
+++ gatsby-starter-default/gatsby-browser.js
@@ -5,6 +5,7 @@
  */
 
 // You can delete this file if you're not using it
+import "@fontsource/open-sans"
 import "@fontsource/merriweather-sans"
 import "@fontsource/merriweather"
 import "@fontsource/roboto"
