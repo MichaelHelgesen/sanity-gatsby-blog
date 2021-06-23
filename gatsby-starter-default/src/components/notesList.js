194ea145e8c80b23ae7dd646eb1b6a6b852c1820
Michael Helgesen <michaelhelgesen02@gmail.com>
2021-06-23 11:59:02 +0200

    Styled "notes"

--- gatsby-starter-default/src/components/notesList.js
+++ gatsby-starter-default/src/components/notesList.js
@@ -5,13 +5,15 @@ import * as style from "../components/notesList.module.scss"
 
 const Notes = ({ props }) => (
     <div className={style.content}>
-    {props.map(post => (
-        <div key={post.node.title} className={style.noteWrap}>
-            <h2 className={style.title}>{post.node.date}</h2>
-            <p style={{ margin: "10px 0 10px 0" }}><BlockContent blocks={post.node._rawText} serializers={serializers} /></p>
-        </div>
+        {props.map(post => (
+            <div key={post.node.title} className={style.noteWrap}>
+                <div className={style.contentWrap}>
+                    <h2 className={style.title}>{post.node.date}</h2>
+                    <p><BlockContent blocks={post.node._rawText} serializers={serializers} /></p>
+                </div>
+            </div>
 
-    ))}
+        ))}
     </div>
 )
 
