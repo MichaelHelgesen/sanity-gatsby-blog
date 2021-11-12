c137c8c8068c23ddd350c808899aa345db7e8336
Michael Helgesen <michaelhelgesen02@gmail.com>
2021-11-12 14:40:19 +0100

    updates

--- gatsby-starter-default/plugins/cloudinary/gatsby-node.js
+++ gatsby-starter-default/plugins/cloudinary/gatsby-node.js
@@ -1,9 +1,9 @@
-let cloudinary = require("cloudinary")
+let cloudinary = require("cloudinary").v2
 
 cloudinary.config({
-    cloud_name: "mikkesblogg",
-    api_key: "689568281515846",
-    api_secret: "RAb_aIzf7cFfjQxZWn4SUBcOUYI",
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET,
     secure: true
 })
 
@@ -11,7 +11,7 @@ cloudinary.config({
 exports.sourceNodes = async function ({ actions, createNodeId, createContentDigest }) {
     const { createNode } = actions
 
-    await cloudinary.v2.search.expression("folder:mikkesblogg/*")
+    await cloudinary.search.expression("folder:mikkesblogg/*")
         .sort_by("created_at", "asc")
         .max_results(200)
         .execute()
