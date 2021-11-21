fe3a6dfc97b241e7943392f5ac1361844557a200
Michael Helgesen <michaelhelgesen02@gmail.com>
2021-11-21 21:51:17 +0100

    updates

--- gatsby-starter-default/gatsby-config.js
+++ gatsby-starter-default/gatsby-config.js
@@ -17,6 +17,7 @@ module.exports = {
     `gatsby-plugin-image`,
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
+    `gatsby-plugin-netlify`,
     `cloudinary`,
     {
       resolve: `gatsby-source-filesystem`,
